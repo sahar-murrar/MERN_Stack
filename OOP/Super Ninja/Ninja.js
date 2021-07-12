9cfc32344106f85db9a250be21d022d8a5db1b61
sahar_murrar <murrarsahar@gmail.com>
2021-07-12 13:42:28 +0300

    Super Ninja Assignment/ Java Script OOP

--- OOP/Super Ninja/Ninja.js
+++ OOP/Super Ninja/Ninja.js
@@ -13,9 +13,29 @@ class Ninja {
         console.log(`Ninja's name: ${ this.name }, strength=  ${ this.strength }, speed= ${ this.speed } and health= ${ this.health }`);
     }
     drinkSake(){
-        this.health+=10;
+        return this.health+=10;
+    }
+}
+class Sensi extends Ninja{
+    constructor(name) {
+        super(name, 200);
+        this.wisdom = 10;
+        this.speed = 10;
+        this.strength = 10;
+    }
+
+    speakWisdom(){
+        const message= super.drinkSake();
+        console.log(`Wisdom message is: ${ message }`);
     }
 }
 const ninja1 = new Ninja("sahar", 100);
 ninja1.sayName();
 ninja1.showState();
+console.log("*******************");
+// example output
+const superSensei = new Sensi("Sahar Murrar");
+superSensei.speakWisdom();
+// -> "What one programmer can do in one month, two programmers can do in two months."
+superSensei.showState();
+// -> "Name: Master Splinter, Health: 210, Speed: 10, Strength: 10"
