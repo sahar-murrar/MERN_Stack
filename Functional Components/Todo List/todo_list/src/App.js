64392a9713315706ede3f5f0e12f3631ab89442a
sahar_murrar <murrarsahar@gmail.com>
2021-07-25 11:10:41 +0300

    Todo List Assignment using react lifting

---
+++ Functional Components/Todo List/todo_list/src/App.js
@@ -0,0 +1,50 @@
+import AddTodo from './components/AddTodo';
+import  TaskForm from './components/TaskForm';
+import React,{useState} from 'react';
+import './App.css';
+
+function App() {
+const [allTasks,setAllTasks]=useState([]);
+
+
+const addToList=(task)=>{
+  if(task!==''){
+    setAllTasks([...allTasks,{taskName:task,checked:false}]) 
+  }
+
+};
+
+const deleteTask=(e,index)=>{
+  setAllTasks([...allTasks.slice(0,index), ...allTasks.slice(index+1)]);// or use ---> const filterdtodos=todos.filter((todo,i)=>{
+};
+
+
+const  taskDone=(e,index)=>{
+  const updatedTasks=allTasks.map((task,idx)=>{
+if(idx===index){
+  task.checked=!task.checked;
+}
+return task; //it is very improtant to return the task that the user checked/ completed it
+  });
+  setAllTasks(updatedTasks);
+}
+
+  return (
+  <div className="App">
+  <TaskForm newTask={addToList}/>
+  <>
+    {
+      allTasks.map((task_name,idx)=>{
+        
+        return (
+        <AddTodo  task={task_name} taskDone={taskDone} delete={deleteTask} index={idx}  />
+        );
+      })
+    }
+  </>
+
+  </div>
+  );
+}
+
+export default App;
---
+++ Functional Components/Todo List/todo_list/src/components/AddTodo.js
@@ -0,0 +1,19 @@
+import './Style/style.css';
+const AddTodo=(props)=>{
+    const class_name=[];
+    if(props.task.checked){
+      class_name.push("crossed-line")
+
+    }
+    return(
+        <div>
+        <input type="checkbox" onChange={(e)=>{
+            props.taskDone(e, props.index)
+        }}  
+        checked={props.task.checked}></input>
+        <label className={class_name}>{props.task.taskName}</label>
+        <button class="button" onClick={(e)=>{props.delete(e,props.index)}} >Delete Task</button>
+    </div>
+    );
+}
+export default AddTodo;
