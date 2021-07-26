56153fb325474d6a4e546cb9a88d7a353ec8bf1c
sahar_murrar <murrarsahar@gmail.com>
2021-07-26 13:38:44 +0300

    Routing Practice Assignment using reach routing and parameters

--- React Routing/Routing Practice/routing_practice/src/components/ParamsComponent.js
+++ React Routing/Routing Practice/routing_practice/src/components/ParamsComponent.js
@@ -3,7 +3,6 @@ import React from 'react'
 const ParamsComponent = (props) => {
     return (
         <div>
-            {/* <p>{check}</p> */}
             {isNaN(+props.param)?"The word is: "+props.param: "The number is: "+props.param}
            
         </div>
